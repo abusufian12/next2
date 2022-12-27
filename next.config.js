b02f99376b368150bd3c8d706c596ed829920eaf
DESKTOP-RLQEV9L\sufian <abusufian12@gmail.com>
2022-12-27 09:05:07 +0900

    up

--- next.config.js
+++ next.config.js
@@ -8,16 +8,16 @@
 
 //const withPWA = require('next-pwa');
 //const runtimeCaching = require('next-pwa/cache');
-const { i18n } = require('./next-i18next.config');
+//const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: false,
-  i18n,
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    //runtimeCaching,
-  },
+  //i18n,
+  // pwa: {
+  //   disable: process.env.NODE_ENV === 'development',
+  //   dest: 'public',
+  //   runtimeCaching,
+  // },
   images: {
     domains: [
       'pickbazarlaravel.s3.ap-southeast-1.amazonaws.com',
